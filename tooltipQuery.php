56c0fbd3543336471e9727f747a88c9128dfafd4
Clau Dermont <clau.dermont@protonmail.ch>
2018-11-23 22:20:11 +0100

    add phpquery, csv data

---
+++ tooltipQuery.php
@@ -0,0 +1,18 @@
+<?php
+include "config.php";
+include "SqlQueryGenerator.php";
+
+$pled = @$_GET['pled'];
+$direcziun = @$_GET['direcziun'];
+$modus = @$_GET['modus'];
+
+$colligiaziun = mysqli_connect("localhost", $mysql_user, $mysql_pass, $mysql_db);
+$SQLString = SqlQueryGenerator::generateDeQuery($pled, $direcziun, $modus);
+
+$Ergebnis = mysqli_query($colligiaziun, $SQLString);
+
+$field = $Ergebnis->fetch_array();
+
+print json_encode($field); 
+
+?>
